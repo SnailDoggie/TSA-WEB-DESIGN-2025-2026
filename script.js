fe1d4dbf289f434fe832bf34da6ab0b4df1f2689
SnailDoggie <bakkacla000@edmonds15.org>
2026-01-20 21:59:34 -0800

    1.20.26
    FINISHED SEARCH!!!!!!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const searchBAR = document.getElementByID('searchBAR');
+const searchBAR = document.getElementById('searchBAR');
 searchBAR.addEventListener('keyup', e =>{
     let currentValue = e.target.value.toLowerCase();
-    console.log(currentValue);
-})
+    let resources = document.querySelectorAll('h1.display-5');
+    resources.forEach(resource => {
+        if (resource.textContent.toLowerCase().includes(currentValue)) {
+            resource.parentNode.parentNode.parentNode.style.display = 'block'; 
+
+        } else {
+            resource.parentNode.parentNode.parentNode.style.display = 'none';
+        }
+    });
+});
